3e0e6a1e1850f7c8ca43b9e78438253f0a405ea6
Kimayoung <157570693+gdhsf3701@users.noreply.github.com>
2025-04-04 12:53:40 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+
+	double a, b; 
+	cin >> a >> b;
+	double result = a / b;
+	cout << fixed;
+	cout.precision(10);
+	cout << result << endl;
+
+	return 0;
+}
+
+
